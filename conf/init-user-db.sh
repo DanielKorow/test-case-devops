788494a24b233502a354bdbc7d72267bb1cc3cd6
Ivan Kroovenkov <ikorovenkov@enegrocomm.ru>
2022-12-15 14:13:16 +0300

    TASK-1: Initial commit

---
+++ conf/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER fluentbit WITH PASSWORD 'fluentbit';
+	CREATE DATABASE fluentbit;
+	GRANT ALL PRIVILEGES ON DATABASE fluentbit TO fluentbit;
+EOSQL
---
+++ up.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir db_data
+docker-compose up --detach
