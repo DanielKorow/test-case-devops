3d64b78c83da9bfd0654c6d1a6a7dc5541e72e99
Ivan Kroovenkov <ikorovenkov@enegrocomm.ru>
2022-12-16 16:48:23 +0300

    TASK-2: Slack notifications was added

--- app/app.py
+++ app/app.py
@@ -2,28 +2,50 @@ import json
 import logging
 import random
 import time
- 
-logging.basicConfig(
-    level="INFO",
-    format="%(asctime)s — %(name)s — %(levelname)s — %(message)s",
-)
-logger = logging.getLogger(__name__)
+
+
+class TestCase:
+    
+    def __init__(self):
+        logging.basicConfig(
+        level="INFO",
+        format="%(asctime)s — %(name)s — %(levelname)s — %(message)s",
+        )
+        self.logger = logging.getLogger(__name__)
+    
+    def bid_ask(self):
+        while True:
+            msg = dict()
+            for level in range(50):
+                (
+                    msg[f"bid_{str(level).zfill(2)}"],
+                    msg[f"ask_{str(level).zfill(2)}"],
+                ) = (
+                    random.randrange(1, 100),
+                    random.randrange(100, 200),
+                )
+            msg["stats"] = {
+                "sum_bid": sum(v for k, v in msg.items() if "bid" in k),
+                "sum_ask": sum(v for k, v in msg.items() if "ask" in k),
+            }
+            self.logger.info(f"{json.dumps(msg)}")
+            if msg['ask_01'] + msg['bid_01'] < 105:
+                self.slack_notification()
+            time.sleep(0.001)
+
+    def slack_notification(self):
+        '''
+        Что бы включить уведосления, нужен токен слака
+        в Dockerfile раскомментировать RUN pip install slack-notifications
+        
+        import os
+        import slack_notifications as slack
+        
+        slack.ACCESS_TOKEN = 'xxx'
+        slack.send_notify('channel-name', username='Bot', text='Alert: ask01+bid01<105')
+        '''
+        print("ALERT!")
  
  
 if __name__ == "__main__":
-    while True:
-        msg = dict()
-        for level in range(50):
-            (
-                msg[f"bid_{str(level).zfill(2)}"],
-                msg[f"ask_{str(level).zfill(2)}"],
-            ) = (
-                random.randrange(1, 100),
-                random.randrange(100, 200),
-            )
-        msg["stats"] = {
-            "sum_bid": sum(v for k, v in msg.items() if "bid" in k),
-            "sum_ask": sum(v for k, v in msg.items() if "ask" in k),
-        }
-        logger.info(f"{json.dumps(msg)}")
-        time.sleep(0.001)
+    TestCase().bid_ask()
